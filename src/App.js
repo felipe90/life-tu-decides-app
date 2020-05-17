d11a311cbe20400ded884ba945e1ce494697353d
Felipe Rojas <felipe90shock@gmail.com>
2020-05-17 18:18:51 -0500

    Added BK img to start page

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <StartPage title={this.state.screens[0].type}></StartPage>
+        <StartPage title={this.state.screens[0].title}></StartPage>
       </div>
     );
   }
--- src/pages/StartPage/StartPage.js
+++ src/pages/StartPage/StartPage.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./StartPage.scss";
 import PlayButton from "../../components/PlayButton/PlayButton";
 import Radium from "radium";
+// import Background from "../../../public/img/background_image.png";
+// import Background from "/background_image.png";
 
 class StartPage extends Component {
   startApp = () => {
@@ -9,8 +11,14 @@ class StartPage extends Component {
   };
 
   render() {
+    const styles = {
+      width: "100%",
+      height: "400px",
+      //   backgroundImage: `url(${Background})`,
+    };
+
     return (
-      <div className="StartPage">
+      <div style={styles} className="StartPage">
         <h1>{this.props.title}</h1>
         <PlayButton click={this.startApp}></PlayButton>
       </div>
