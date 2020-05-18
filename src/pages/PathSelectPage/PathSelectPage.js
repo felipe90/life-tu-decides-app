7fbba5f892c5cd495c913989e3dcf1156e9ebb03
Felipe Rojas <felipe90shock@gmail.com>
2020-05-18 01:33:42 -0500

    Added video player

--- src/pages/PathSelectPage/PathSelectPage.js
+++ src/pages/PathSelectPage/PathSelectPage.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./PathSelectPage.scss";
 import Radium from "radium";
+import ReactPlayer from "react-player";
 
 const pathSelectPage = (props) => {
-  return <div className="PathSelectPage">path select</div>;
+  return (
+    <div className="PathSelectPage">
+      <ReactPlayer url="/reference/demo-video.mp4" controls={true} />
+    </div>
+  );
 };
 
 export default Radium(pathSelectPage);
